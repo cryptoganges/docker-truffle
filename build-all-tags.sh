8a7bf39485a70666e9334a8758a7460d0a87b6d3
Nathan <nathan@cryptoganges.com>
2021-01-17 21:01:01 -0700

    Add multi-architecture build

--- build-all-tags.sh
+++ build-all-tags.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect --bootstrap
+
 NODE_TAGS=(15-alpine 15-buster)
 TRUFFLE_VERSIONS=(5.1.62)
 
@@ -11,8 +15,11 @@ do
      :
      TAG="nathancryptoganges/truffle:$nodeTag-$truffleVersion"
      echo "Building docker image tag $TAG for node $nodeTag and truffle $truffleVersion"
-     DOCKER_BUILD_COMMAND="docker build --build-arg NODE_BASE_IMAGE=node:$nodeTag --build-arg TRUFFLE_VERSION=$truffleVersion --no-cache --rm -t $TAG ."
+     DOCKER_BUILD_COMMAND="docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg NODE_BASE_IMAGE=node:$nodeTag --build-arg TRUFFLE_VERSION=$truffleVersion --no-cache --rm -t $TAG --push ."
      echo $DOCKER_BUILD_COMMAND
      $DOCKER_BUILD_COMMAND
    done  
 done
+
+docker buildx stop mybuilder
+docker buildx rm mybuilder
