375e696de8afc088be1a2c026acc1f7633011669
Nathan <nathan@cryptoganges.com>
2021-01-17 20:08:01 -0700

    Updated docker repository name

--- build-all-tags.sh
+++ build-all-tags.sh
@@ -9,12 +9,10 @@ do
    for truffleVersion in "${TRUFFLE_VERSIONS[@]}"
    do
      :
-     TAG="cryptoganges/truffle:$nodeTag-$truffleVersion"
+     TAG="nathancryptoganges/truffle:$nodeTag-$truffleVersion"
      echo "Building docker image tag $TAG for node $nodeTag and truffle $truffleVersion"
      DOCKER_BUILD_COMMAND="docker build --build-arg NODE_BASE_IMAGE=node:$nodeTag --build-arg TRUFFLE_VERSION=$truffleVersion --no-cache --rm -t $TAG ."
      echo $DOCKER_BUILD_COMMAND
      $DOCKER_BUILD_COMMAND
    done  
 done
-
-#wget -q https://registry.hub.docker.com/v1/repositories/node/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'
